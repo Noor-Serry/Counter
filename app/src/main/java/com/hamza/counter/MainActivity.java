981027d68adb7dd333157c0e9abb1d045360423a
Noor Serry <nour.serry.94@gmail.com>
2022-01-02 12:21:02 +0200

    set(glide) to splashScreen

--- app/src/main/java/com/hamza/counter/MainActivity.java
+++ app/src/main/java/com/hamza/counter/MainActivity.java
@@ -5,12 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,27 +15,22 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView countertextview,total,maxValue;
+    TextView countertextview, totalView, maxValue;
     int counter = 0;
-
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         countertextview = findViewById(R.id.counterTextview);
-       total=findViewById(R.id.Total);
-       maxValue=findViewById(R.id.Max);
-
+        totalView = findViewById(R.id.Total);
+        maxValue = findViewById(R.id.Max);
         creatSharedPreferences();
         creatEditor();
-      setTotalInTextView();
-      setMaxValueInTextView();
-
+        setTotalInTextView();
+        setMaxValueInTextView();
 
     }
 
@@ -47,12 +38,17 @@ public class MainActivity extends AppCompatActivity {
         counter++;
         checkCounter();
         countertextview.setText(String.valueOf(counter));
+       saveUpdateIncreaseCounter();
     }
-
+    public void saveUpdateIncreaseCounter(){
+        setTotalValesInSharedPreferences();
+        setTotalInTextView();
+        setMaxValueInSharedPreferences();
+        setMaxValueInTextView();
+    }
     private void checkCounter() {
         if (counter % 10 == 0)
             countertextview.setTextColor(getRandomcolor());
-
     }
 
     private int getRandomcolor() {
@@ -60,80 +56,78 @@ public class MainActivity extends AppCompatActivity {
         int random = new Random().nextInt(colors.length);
         return colors[random];
     }
-
-
     public void resetCounter(View view) {
-        counter = 0;
-        countertextview.setText(String.valueOf(counter));
-        countertextview.setTextColor(Color.BLACK);
+      reset();
     }
-    public void creatSharedPreferences(){
-        sharedPreferences=getSharedPreferences("counterValue",MODE_PRIVATE);
+public void reset(){
+    counter = 0;
+    countertextview.setText(String.valueOf(counter));
+    countertextview.setTextColor(Color.BLACK);
+}
+    public void creatSharedPreferences() {
+        sharedPreferences = getSharedPreferences("counterValue", MODE_PRIVATE);
     }
-    public void creatEditor(){
+    public void creatEditor() {
         editor = sharedPreferences.edit();
     }
-   public void setTotalInTextView(){
-        int totalInFile = sharedPreferences.getInt("Total",0);
-        total.setText(getString(R.string.Total)+totalInFile);
-   }
-   public void setMaxValueInTextView(){
-        int maxInFile = sharedPreferences.getInt("Max",0);
-        maxValue.setText(getString(R.string.MaxValue)+maxInFile);
 
-   }
-public void inFlatMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-}
+    public void setTotalInTextView() {
+        int totalInShared = sharedPreferences.getInt("Total",0);
+        totalView.setText(getString(R.string.Total) +totalInShared);
+    }
+    public void setMaxValueInTextView() {
+        int MaxInShared=sharedPreferences.getInt("Max",0);
+        maxValue.setText(getString(R.string.MaxValue) + MaxInShared);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        inFlatMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
-public void deletTotal(){
-    editor.putInt("Total",0);
-    editor.apply();
-    setTotalInTextView();
-}
-public void deletMaxValue(){
-    editor.putInt("Max",0);
-    editor.apply();
-    setMaxValueInTextView();
-}
-public void selectSelectionFromMenu(MenuItem item){
-        switch (item.getItemId()){
+    public void selectSelectionFromMenu(MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.deletTotal:
                 deletTotal();
-                Toast.makeText(getBaseContext(), getString(R.string.TotalDelet), Toast.LENGTH_SHORT).show();break;
-            case R.id.deletMax: deletMaxValue();
+                Toast.makeText(getBaseContext(), getString(R.string.TotalDelet), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.deletMax:
+                deletMaxValue();
                 Toast.makeText(getBaseContext(), getString(R.string.MaxDelet), Toast.LENGTH_SHORT).show();
-        } }
+        }
+    }
+    public void deletTotal() {
+        editor.putInt("Total", 0);
+        editor.apply();
+        setTotalInTextView();
+    }
+    public void deletMaxValue() {
+        editor.putInt("Max", 0);
+        reset();
+        editor.apply();
+        setMaxValueInTextView();
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         selectSelectionFromMenu(item);
         return super.onOptionsItemSelected(item);
     }
-
     @Override
     protected void onStop() {
         super.onStop();
-        setTotalVales();
-        setMaxValue();
+        setMaxValueInSharedPreferences();
+    }
+
+    public void setTotalValesInSharedPreferences() {
+        editor.putInt("Total",sharedPreferences.getInt("Total",0)+ +1);
         editor.apply();
     }
-    public void setTotalVales(){
-        editor.putInt("Total",sharedPreferences.getInt("Total",0)+counter);
-    }
-    public void setMaxValue(){
-        if(sharedPreferences.getInt("Max",0)<counter){
-            editor.putInt("Max",counter);
+
+    public void setMaxValueInSharedPreferences() {
+        if (sharedPreferences.getInt("Max", 0) < counter) {
+            editor.putInt("Max", counter);
+            editor.apply();
         }
 
     }
-
-
-
-
 
 }
--- app/src/main/java/com/hamza/counter/SplashScreen.java
+++ app/src/main/java/com/hamza/counter/SplashScreen.java
@@ -7,31 +7,40 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.bumptech.glide.Glide;
 
 
 public class SplashScreen extends AppCompatActivity {
 TextView welcomeText;
 int timeSleep = 5000;
     Animation animation;
+    ImageView messagePhoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         welcomeText=findViewById(R.id.welcomView);
+        messagePhoto=findViewById(R.id.message);
         setAnimationToText();
+        addGlide();
          sleepActivity();
 
     }
 
     public void setAnimationToText(){
-        animation =  AnimationUtils.loadAnimation(getBaseContext(),R.anim.text_anim);
+        animation = AnimationUtils.loadAnimation(getBaseContext(),R.anim.text_anim);
         welcomeText.setAnimation(animation);
     }
+    public void addGlide(){
+        Glide.with(getBaseContext()).load(R.drawable.se).into(messagePhoto);
+    }
+
 public void sleepActivity(){
  new Handler().postDelayed(new Runnable() {@Override public void run() { goToMainActivite(); }},timeSleep);
 }
@@ -39,8 +48,7 @@ public void goToMainActivite(){
     Intent intent = new Intent(getBaseContext(),MainActivity.class);
     startActivity(intent);
     finish();
-}
+}}
 
-}
 
 
